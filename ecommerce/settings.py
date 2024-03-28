39e824fc34c7315c31525e24a8176606e50e4986
Anish Gharti <anishgharti10@gmail.com>
2024-03-28 15:42:13 +0545

    4th commit

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,4 @@ STATICFILES_DIRS = [BASE_DIR / 'ecommerce/static']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.Account'
